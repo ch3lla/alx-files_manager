55f58dc7ebd593c52e78e1df1ecfe47c678b445b
ch3lla <chukwumaemmanuella03@gmail.com>
2024-02-12 12:53:24 +0100

    task 4

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -7,9 +7,9 @@ const userQueue = new Queue('email sending');
 
 export default class UsersController {
   static async postNew(req, res) {
-    // const email = req.body ? req.body.email : null;
-    // const password = req.body ? req.body.password : null;
-    const { email, password } = req.body;
+    const email = req.body ? req.body.email : null;
+    const password = req.body ? req.body.password : null;
+
 
     if (!email) {
       res.status(400).json({ error: 'Missing email' });
