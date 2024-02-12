f4a6365177f5941d0c824f8ae77e67254c101945
ch3lla <chukwumaemmanuella03@gmail.com>
2024-02-12 13:31:08 +0100

    task 7

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,8 @@ import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
 import { basicAuthenticate, xTokenAuthenticate } from '../middlewares/auth';
-/* import FilesController from '../controllers/FilesController.js';
-import { APIError, errorResponse } from '../middlewares/error.js'; */
+import FilesController from '../controllers/FilesController.js';
+/*import { APIError, errorResponse } from '../middlewares/error.js'; */
 
 
 /**
@@ -21,11 +21,12 @@ const injectRoutes = (api) => {
   api.get('/connect', basicAuthenticate, AuthController.getConnect);
   api.get('/disconnect', xTokenAuthenticate, AuthController.getDisconnect);
   api.get('/users/me', xTokenAuthenticate, UsersController.getMe);
-/*
+
   api.post('/files', xTokenAuthenticate, FilesController.postUpload);
+
   api.get('/files/:id', xTokenAuthenticate, FilesController.getShow);
   api.get('/files', xTokenAuthenticate, FilesController.getIndex);
-  api.put('/files/:id/publish', xTokenAuthenticate, FilesController.putPublish);
+/*  api.put('/files/:id/publish', xTokenAuthenticate, FilesController.putPublish);
   api.put('/files/:id/unpublish', xTokenAuthenticate, FilesController.putUnpublish);
   api.get('/files/:id/data', FilesController.getFile); */
 
